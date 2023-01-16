04ba622139960c843ea002e9286c00cac8715393
p-rayne <marlinaqua@gmail.com>
2023-01-16 20:24:36 +0300

    edit UA

--- level_0/scraper.py
+++ level_0/scraper.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 from time import sleep
 
 headers = {
-    "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36"
+    "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36"
 }
 
 
