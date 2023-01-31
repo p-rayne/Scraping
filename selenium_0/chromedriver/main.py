7622a3a14266d4898250bd17bd01e733e01494eb
p-rayne <marlinaqua@gmail.com>
2023-01-31 17:19:31 +0300

    added proxy

--- selenium_0/chromedriver/main.py
+++ selenium_0/chromedriver/main.py
@@ -1,7 +1,11 @@
 import time
 import random
-from selenium import webdriver
+
+# from selenium import webdriver
+from seleniumwire import webdriver
 from fake_useragent import UserAgent
+
+from proxy_auth_data import login, password
 
 # url = "https://www.python.org/"
 
@@ -19,15 +23,28 @@ options = webdriver.ChromeOptions()
 # options.add_argument(f"user-agent={useragent.opera}")
 options.add_argument(f"user-agent={useragent.random}")
 
+# set proxy
+# options.add_argument("--proxy-server=138.128.91.65:8000")
+
+proxy_options = {"proxy": {"https": f"http://{login}:{password}@138.128.91.65:8000"}}
+
+# driver = webdriver.Chrome(
+#     executable_path="/home/p-rayne/Python/Learning/Scraping/selenium_0/chromedriver/chromedriver",
+#     options=options,
+# )
+
 driver = webdriver.Chrome(
     executable_path="/home/p-rayne/Python/Learning/Scraping/selenium_0/chromedriver/chromedriver",
-    options=options,
+    seleniumwire_options=proxy_options,
 )
 
 try:
-    driver.get(url="https://www.whatismybrowser.com/detect/what-is-my-user-agent")
-    time.sleep(5)
+    # driver.get(url="https://www.whatismybrowser.com/detect/what-is-my-user-agent")
+    # time.sleep(5)
     # driver.get_screenshot_as_file("1.png")
+
+    driver.get("https://2ip.ru")
+    time.sleep(5)
 except Exception as ex:
     print(ex)
 finally:
--- selenium_0/firefoxdriver/main.py
+++ selenium_0/firefoxdriver/main.py
@@ -1,6 +1,8 @@
-from selenium import webdriver
+# from selenium import webdriver
+from seleniumwire import webdriver
 import time
 from fake_useragent import UserAgent
+from proxy_auth_data import login, password
 
 # url = "https://www.python.org/"
 
@@ -13,13 +15,36 @@ options = webdriver.FirefoxOptions()
 # options.set_preference("general.useragent.override", "hello World")
 options.set_preference("general.useragent.override", useragent.random)
 
+# set proxy
+# proxy = "138.128.91.65:8000"
+# firefox_capabilities = webdriver.DesiredCapabilities.FIREFOX
+# firefox_capabilities["marionette"] = True
+
+# firefox_capabilities["proxy"] = {
+#     "proxyType": "MANUAL",
+#     "httpProxy": proxy,
+#     "ftpProxy": proxy,
+#     "sslProxy": proxy,
+# }
+
+proxy_options = {"proxy": {"https": f"http://{login}:{password}@138.128.91.65:8000"}}
+
+# driver = webdriver.Firefox(
+#     executable_path="/home/p-rayne/Python/Learning/Scraping/selenium_0/firefoxdriver/geckodriver",
+#     options=options,
+#     proxy=proxy,
+# )
+
 driver = webdriver.Firefox(
     executable_path="/home/p-rayne/Python/Learning/Scraping/selenium_0/firefoxdriver/geckodriver",
-    options=options,
+    seleniumwire_options=proxy_options,
 )
 
 try:
-    driver.get(url="https://www.whatismybrowser.com/detect/what-is-my-user-agent")
+    # driver.get(url="https://www.whatismybrowser.com/detect/what-is-my-user-agent")
+    # time.sleep(5)
+
+    driver.get("https://2ip.ru")
     time.sleep(5)
 except Exception as ex:
     print(ex)
