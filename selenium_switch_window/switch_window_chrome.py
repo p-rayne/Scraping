d05e4d376534a6c297891a432564b2929ed6ef8b
p-rayne <marlinaqua@gmail.com>
2023-02-01 18:49:24 +0300

    time.sleep change to implicitly_wait

--- selenium_switch_window/switch_window_chrome.py
+++ selenium_switch_window/switch_window_chrome.py
@@ -22,37 +22,38 @@ driver = webdriver.Chrome(
 
 
 try:
+    driver.implicitly_wait(5)
     driver.get(
         "https://www.avito.ru/moskva/tovary_dlya_kompyutera/komplektuyuschie/videokarty-ASgBAgICAkTGB~pm7gmmZw?cd=1"
     )
     # print(driver.window_handles)
     print(f"Currently URL is: {driver.current_url}")
-    time.sleep(5)
+    # time.sleep(5)
 
     items = driver.find_elements("xpath", "//div[@data-marker='item-photo']")
     items[0].click()
     # print(driver.window_handles)
 
-    time.sleep(5)
+    # time.sleep(5)
 
     driver.switch_to.window(driver.window_handles[1])
-    time.sleep(5)
+    # time.sleep(5)
     print(f"Currently URL is: {driver.current_url}")
 
     username = driver.find_element("xpath", "//div[@data-marker='seller-info/name']")
     print(f"Username is: {username.text}")
-    time.sleep(5)
+    # time.sleep(5)
 
     driver.close()
     driver.switch_to.window(driver.window_handles[0])
-    time.sleep(5)
+    # time.sleep(5)
     print(f"Currently URL is: {driver.current_url}")
 
     items[1].click()
-    time.sleep(5)
+    # time.sleep(5)
 
     driver.switch_to.window(driver.window_handles[1])
-    time.sleep(5)
+    # time.sleep(5)
     print(f"Currently URL is: {driver.current_url}")
     price = driver.find_element(
         "css selector",
